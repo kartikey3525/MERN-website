6389e04b712e4726b632b0eca744e55be64eeafc
kartikey <kartikey.kapoor@nesindia.co>
2026-01-13 12:24:27 +0530

    Fix: auth changes for production

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-// import { AuthProvider } from "./store/auth.jsx";
+import { AuthProvider } from "./store/auth.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
